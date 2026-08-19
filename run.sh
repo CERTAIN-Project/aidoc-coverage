e383453b0262b72d83fd6db38859bbc7d442cf63
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2026-08-19 13:50:52 +0200

    shorter docker run command

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 3000:3000 -e HOST=0.0.0.0 -e PORT=3000 -e SPARQL_ENDPOINT_URL=http://ontop:8080/sparql coverage
+docker run --rm -p 3000:3000 -e SPARQL_ENDPOINT_URL=http://ontop:8080/sparql coverage
